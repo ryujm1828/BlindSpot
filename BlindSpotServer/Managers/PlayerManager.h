13982fe5bd58f9aef20a1474b93f64a390964da8
ryujm1828 <ryujm1828@gmail.com>
2026-01-28 08:25:48 +0900

    [refactor] Following SRP principle

--- BlindSpotServer/Managers/PlayerManager.h
+++ BlindSpotServer/Managers/PlayerManager.h
@@ -15,14 +15,8 @@ class PlayerManager {
     static std::map<int32_t, std::string> playerIdToName_;
 public:
     static PlayerManager& Instance();
-
     int32_t GeneratePlayerId();
-
     void RegisterPlayerName(int32_t playerId, const std::string& name);
-    
     void EditPlayerName(int32_t playerId, const std::string& newName);
-
     std::string GetPlayerNameById(int32_t playerId);
-    
-
 };
